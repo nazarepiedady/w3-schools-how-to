d0b0ddd08fd18d87bdd518449da96a8c5201fb0b
Nazaré da Piedade <nazarepiedady@gmail.com>
2022-04-30 21:17:23 +0100

    feat: create the function `openCity(event, cityName)`
    it will remove the `active` class and set `none` to display for
    all element with them and then reset the `active` class and display
    `block` to the current clicked interactive element.

---
+++ tabs/scripts.js
@@ -0,0 +1,14 @@
+function openCity(event, cityName) {
+  var $tabcontent = document.getElementsByClassName('tabcontent');
+  for (var index = 0; index < $tabcontent.length; index++) {
+    $tabcontent[index].style.display = 'none';
+  }
+
+  var $tablinks = document.getElementsByClassName('tablinks');
+  for (var index = 0; index < $tablinks.length; index++) {
+    $tablinks[index].className = $tablinks[index].className.replace(' active', '');
+  }
+
+  event.currentTarget.className += ' active';
+  document.getElementById(cityName).style.display = 'block';
+}
