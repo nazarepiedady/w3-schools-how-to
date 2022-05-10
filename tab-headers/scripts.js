aa8604fd66475224e19994ad87e1edb61a224990
Nazaré da Piedade <nazarepiedady@gmail.com>
2022-05-10 23:22:12 +0100

    chore: create openCity function to alternate between cities tabs
    it will reveal the content section that match the current clicked
    `tablink` component.

---
+++ tab-headers/scripts.js
@@ -0,0 +1,14 @@
+function openCity(cityName, $element, color) {
+  var $tabcontent = document.getElementsByClassName('tabcontent');
+  for (var index = 0; index < $tabcontent.length; index++) {
+    $tabcontent[index].style.display = 'none';
+  }
+
+  var $tablinks = document.getElementsByClassName('tablink');
+  for (var index = 0; index < $tablinks.length; index++) {
+    $tablinks[index].style.backgroundColor = '';
+  }
+
+  document.getElementById(cityName).style.display = 'block';
+  $element.style.backgroundColor = color;
+}
