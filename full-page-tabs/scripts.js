9310a44b2e29b43af806ef0b5a7feb1cacb81674
Nazaré da Piedade <nazarepiedady@gmail.com>
2022-05-12 19:01:07 +0100

    chore: iterate over `tabcontent` components to set their display
    it will select one by one each components and set their display to
    `none` to hide them from users and screen readers.

--- full-page-tabs/scripts.js
+++ full-page-tabs/scripts.js
@@ -1,3 +1,6 @@
 function openPage(pageName, $element, color) {
   var $tabcontent = document.getElementsByClassName('tabcontent');
+  for (var index = 0; index < $tabcontent.length; index++) {
+    $tabcontent[index].style.display = 'none';
+  }
 }
