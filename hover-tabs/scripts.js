955abd28be29c0137c874ebdc792c9469ba7244b
Nazaré da Piedade <nazarepiedady@gmail.com>
2022-05-14 19:12:27 +0100

    chore: iterate over `tabcontent` components
    to set all their display property as `none`

--- hover-tabs/scripts.js
+++ hover-tabs/scripts.js
@@ -1,3 +1,6 @@
 function openCity(event, cityName) {
   var $tabcontent = document.getElementsByClassName('tabcontent');
+  for (var index = 0; index < $tabcontent.length; index++) {
+    $tabcontent[index].style.display = 'none';
+  }
 }
