3977f74c5c99af7611443a924d748e92fcd17d91
Nazaré da Piedade <nazarepiedady@gmail.com>
2022-05-28 21:42:46 +0100

    feat: implement the open side navigation function

---
+++ animated-side-navigation-push-opacity/scripts.js
@@ -0,0 +1,5 @@
+function openNav() {
+  document.getElementById('my-sidenav').style.width = '250px';
+  document.getElementById('main').style.marginLeft = '250px';
+  document.body.style.backgroundColor = 'rgba(0, 0, 0, 0.4)';
+}
