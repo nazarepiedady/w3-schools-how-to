d935130fe25f07c038beb93fd632fa72ea60348b
Nazaré da Piedade <nazarepiedady@gmail.com>
2022-06-02 18:21:27 +0100

    feat: implement the deactivate push right animation function

--- animated-fullscreen-overlay-navigation/scripts.js
+++ animated-fullscreen-overlay-navigation/scripts.js
@@ -3,6 +3,11 @@ function activatePushRightAnimation() {
   $overlay.style.width = '100%';
 }
 
+function deactivatePushRightAnimation() {
+  var $overlay = document.querySelector('.overlay--push-right');
+  $overlay.style.width = '0';
+}
+
 
 function openNav() {
   document.getElementById('navigation').style.width = '100%';
