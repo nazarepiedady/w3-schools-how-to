7448176417d3b428cd57c037fe0c976b7d79b0cb
Nazaré da Piedade <nazarepiedady@gmail.com>
2022-06-07 19:59:49 +0100

    feat: implement the close side navigation function

--- off-canvas-menu-opacity/scripts.js
+++ off-canvas-menu-opacity/scripts.js
@@ -3,3 +3,9 @@ function openSideNavigation() {
   document.getElementById('main').style.marginLeft = '250px';
   document.body.style.backgroundColor = 'rgba(0, 0, 0, 0.4)';
 }
+
+function closeSideNavigation() {
+  document.getElementById('side-navigation').style.width = '0';
+  document.getElementById('main').style.marginLeft = '0';
+  document.body.style.backgroundColor = 'rgba(255, 255, 255, 1)';
+}
