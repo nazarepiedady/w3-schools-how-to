828871a14664e2eda24dd183a859e8f647e38e7e
Nazaré da Piedade <nazarepiedady@gmail.com>
2022-06-26 20:56:45 +0100

    feat: implement the function `showNavbarOnScroll`

--- hide-navbar-on-scroll/scripts.js
+++ hide-navbar-on-scroll/scripts.js
@@ -1,3 +1,12 @@
 var previousScrollPosition = window.pageYOffset;
 
-
+function showNavbarOnScroll() {
+  var $navbar = document.getElementById('navbar');
+  var currentScrollPosition = window.pageYOffset;
+  if (previousScrollPosition > currentScrollPosition) {
+    $navbar.style.top = '0';
+  } else {
+    $navbar.style.top = '-50px';
+  }
+  previousScrollPosition = currentScrollPosition;
+}
