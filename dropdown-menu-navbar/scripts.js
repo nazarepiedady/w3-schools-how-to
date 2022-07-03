e763c9dff680b285e5fb6d33305c05e4a185bd3b
Nazaré da Piedade <nazarepiedady@gmail.com>
2022-07-03 22:31:52 +0100

    feat: implement the `hideDropdownMenu` function

--- dropdown-menu-navbar/scripts.js
+++ dropdown-menu-navbar/scripts.js
@@ -2,3 +2,13 @@ function showDropdownMenu() {
   var $dropdownMenu = document.getElementById('dropdown-menu');
   $dropdownMenu.classList.toggle('show-dropdown-menu');
 }
+
+
+window.onclick = function hideDropdownMenu(event) {
+  if (!event.target.matches('.drop-button')) {
+    var $dropdownMenu = document.getElementById('dropdown-menu');
+    if ($dropdownMenu.classList.contains('show-dropdown-menu')) {
+      $dropdownMenu.classList.remove('show-dropdown-menu');
+    }
+  }
+}
