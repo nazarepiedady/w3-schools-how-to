dc7ee05bd14f01dd4a6d156e921a6b9d87617ede
Nazaré da Piedade <nazarepiedady@gmail.com>
2022-07-04 23:46:39 +0100

    feat: implement the `showDropdownMenu` function to dropdowns on click event

--- sidebar-dropdown/scripts.js
+++ sidebar-dropdown/scripts.js
@@ -1,2 +1,14 @@
 var index = 0;
 var dropdowns = document.getElementsByClassName('dropdown-button');
+
+for (index = 0; index < dropdowns.length; index++) {
+  dropdowns[index].addEventListener('click', function showDropdownMenu() {
+    this.classList.toggle('active');
+    var $dropdownContent = this.nextElementSibling;
+    if ($dropdownContent.style.display === 'block') {
+      $dropdownContent.style.display = 'none';
+    } else {
+      $dropdownContent.style.display = 'block';
+    }
+  });
+}
