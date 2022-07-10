4863f91621effc9ea479ec5742537b3ef8bb2b5f
Nazaré da Piedade <nazarepiedady@gmail.com>
2022-07-10 22:58:11 +0100

    feat: implement the `hideNavigation` function

--- curtain-menu/scripts.js
+++ curtain-menu/scripts.js
@@ -2,3 +2,8 @@ function showNavigation() {
   var $navigation = document.getElementById('navigation');
   $navigation.style.width = '100%';
 }
+
+function hideNavigation() {
+  var $navigation = document.getElementById('navigation');
+  $navigation.style.width = '0';
+}
