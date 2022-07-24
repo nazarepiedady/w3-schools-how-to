35b0b40b843cf5c986d2d62a437384e5bccd624c
Nazaré da Piedade <nazarepiedady@gmail.com>
2022-07-24 19:50:52 +0100

    feat: implement the `showSlide` function

---
+++ slideshow/scripts.js
@@ -0,0 +1,24 @@
+var defaultSlideIndex = 1;
+//showSlide(slideIndex);
+
+
+function showSlide(slideIndex) {
+  var slides = document.getElementsByClassName('slide');
+  var dotButtons = document.getElementsByClassName('dot-button');
+
+  if (slideIndex > slides.length) {
+    defaultSlideIndex = 1;
+  }
+
+  if (slideIndex < 1) {
+    slideIndex = slides.length;
+  }
+
+  for (var index = 0; index < dotButtons.length; index++) {
+    dotButtons[index].className =
+      dotButtons[index].className.replace(' dot-button-active', '');
+  }
+
+  slides[slideIndex - 1].style.display = 'block';
+  dotButtons[slideIndex - 1].className += ' dot-button-active';
+}
