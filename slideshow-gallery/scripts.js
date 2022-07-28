58b353574cf6b443b36818b36c529b758e787d9a
Nazaré da Piedade <nazarepiedady@gmail.com>
2022-07-28 22:12:10 +0100

    feat: create function `showSlide` to show slides item

--- slideshow-gallery/scripts.js
+++ slideshow-gallery/scripts.js
@@ -11,3 +11,29 @@ function nextSlide() {
 function currentSlide(slideIndex) {
   showSlide(slideDefaultIndex = slideIndex);
 }
+
+function showSlide(slideIndex) {
+  var slides = document.getElementsByClassName('slide');
+  var thumbs = document.getElementsByClassName('demo-image');
+  var $slideCaption = document.getElementById('slide-caption');
+
+  if (slideIndex > slides.length) {
+    slideDefaultIndex = 1;
+  }
+
+  if (slideIndex < 1) {
+    slideDefaultIndex = slides.length;
+  }
+
+  for (var index = 0; index < slides.length; index++) {
+    slides[index].style.display = 'none';
+  }
+
+  for (var index = 0; index < thumbs.length; index++) {
+    thumbs[index].className = thumbs[index].className.replace(' active', '');
+  }
+
+  slides[slideDefaultIndex - 1].style.display = 'block';
+  thumbs[slideDefaultIndex - 1].className += ' active';
+  $slideCaption.textContent = thumbs[defaultSlideIndex - 1].alt;
+}
