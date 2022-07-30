dd274305849fd1ea363cc3d68ec1d71d1bfb4be6
Nazaré da Piedade <nazarepiedady@gmail.com>
2022-07-30 20:52:20 +0100

    feat: implement the function `showModal` to the thumbnail image as on click event

---
+++ modal-images/scripts.js
@@ -0,0 +1,12 @@
+var $modal = document.getElementById('modal');
+var $modalImage = document.getElementById('modal-image');
+var $modalCaption = document.getElementById('modal-caption');
+var $thumbnailImage = document.getElementById('thumbnail-image');
+var $modalCloseButton = document.getElementById('modal-close-button');
+
+
+$thumbnailImage.onclick = function showModal() {
+  $modalImage.src = this.src;
+  $modal.style.display = 'block';
+  $modalCaption.textContent = this.alt;
+};
