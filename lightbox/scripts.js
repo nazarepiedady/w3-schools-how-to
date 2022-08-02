3702439cb4ea0211493c3f266c08bbe5bc00e81f
Nazaré da Piedade <nazarepiedady@gmail.com>
2022-08-02 08:36:15 +0100

    feat: write and implement the function `showSlide`

--- lightbox/scripts.js
+++ lightbox/scripts.js
@@ -18,3 +18,29 @@ function previousSlide() {
 function nextSlide() {
   showSlide(slideDefaultIndex += 1);
 }
+
+function showSlide(slideIndex) {
+  var slides = document.getElementsByClassName('slide');
+  var $slideCaption = document.getElementById('slide-caption');
+  var thumbs = document.getElementsByClassName('thumbnail-image');
+
+  if (slideIndex > slides.length) {
+    slideDefaultIndex = 1;
+  }
+
+  if (slideIndex < 1) {
+    slideDefaultIndex = slides.length;
+  }
+
+  for (var index = 0; index < slides.length; index++) {
+    slides[index].style.display = 'none';
+  }
+
+  for (var index = 0; index < thumbs.length; index++) {
+    thumbs[index].clalssName = thumbs[index].className.replace(' active', '');
+  }
+
+  slides[slideDefaultIndex - 1].style.display = 'block';
+  thumbs[slideDefaultIndex - 1].className += 'active';
+  $slideCaption.textContent = thumbs[slideDefaultIndex - 1].alt;
+}
