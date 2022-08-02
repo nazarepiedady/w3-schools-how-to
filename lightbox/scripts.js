991b11d999e0bf3bb1573e133b92a0bae34cb965
Nazaré da Piedade <nazarepiedady@gmail.com>
2022-08-02 08:24:03 +0100

    feat: write and implement the function `closeModal`

--- lightbox/scripts.js
+++ lightbox/scripts.js
@@ -2,3 +2,8 @@ function openModal() {
   var $modal = document.getElementById('modal');
   $modal.style.display = 'block';
 }
+
+function closeModal() {
+  var $modal = document.getElementById('modal');
+  $modal.style.display = 'none';
+}
