c057e8340059dceb69e7703494bc56a5ec56f6d2
Nazaré da Piedade <nazarepiedady@gmail.com>
2022-08-08 20:44:02 +0100

    feat: create and implement the function `startHighlightButton`

--- image-grid/scripts.js
+++ image-grid/scripts.js
@@ -1,6 +1,5 @@
 var columns = document.getElementsByClassName('column');
 
-
 function showOneColumnGrid() {
   var columnsLength = columns.length;
   for (var index = 0; index < columnsLength; index++) {
@@ -21,3 +20,22 @@ function showFourColumnGrid() {
     columns[index].style.flex = '25%';
   }
 }
+
+function startHighlightButton() {
+  var $header = document.getElementById('header');
+  var buttons = $header.getElementsByClassName('button');
+  var buttonsLength = buttons.length;
+
+  function highlightButton() {
+    var $current = $header.getElementsByClassName('active');
+    $current[0].className = $current[0].className.replace(' active', '');
+    this.className += ' active';
+  }
+
+  for (var index = 0; index < buttonsLength; index++) {
+    buttons[index].addEventListener('click', highlightButton);
+  }
+}
+
+
+startHighlightButton();
