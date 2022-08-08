df3cbe3702a79a43125f1883b34f8516ecad26f5
Nazaré da Piedade <nazarepiedady@gmail.com>
2022-08-08 20:28:55 +0100

    feat: create and implement the function `showTwoColumnGrid`

--- image-grid/scripts.js
+++ image-grid/scripts.js
@@ -7,3 +7,10 @@ function showOneColumnGrid() {
     columns[index].style.flex = '100%';
   }
 }
+
+function showTwoColumnGrid() {
+  var columnsLength = columns.length;
+  for (var index = 0; index < columnsLength; index++) {
+    columns[index].style.flex = '50%';
+  }
+}
