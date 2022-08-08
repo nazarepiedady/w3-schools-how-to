800833407d7f316c8fb35476564e3a689ca9c315
Nazaré da Piedade <nazarepiedady@gmail.com>
2022-08-08 20:31:09 +0100

    feat: create and implement the function `showFourColumnGrid`

--- image-grid/scripts.js
+++ image-grid/scripts.js
@@ -14,3 +14,10 @@ function showTwoColumnGrid() {
     columns[index].style.flex = '50%';
   }
 }
+
+function showFourColumnGrid() {
+  var columnsLength = columns.length;
+  for (var index = 0; index < columnsLength; index++) {
+    columns[index].style.flex = '25%';
+  }
+}
